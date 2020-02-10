821f83512e3880e241a1ee2aec6173220b12a823
Ashik Liyakathali <ashik21395@gmail.com>
2020-02-10 14:27:21 -0500

    generalized the setState and handleClick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Coding Test
 
-// A retailer offers a amount program to its customers, awarding points based on each recorded purchase.
+// A retailer offers a amount program to its customers_id_list, awarding points based on each recorded purchase.
 // A customer receives 2 points for every dollar spent over $100 in each transaction,
 // plus 1 point for every dollar spent over $50 in each transaction (e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).
 // Given a record of every transaction during a three month period, 
@@ -13,67 +13,47 @@
 
 import React from 'react';
 import './App.css';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 class App extends React.Component{
   state = {
+    Name: "",
     April: "",
     May: "",
     June: "",
     Total: ""
   }
 
-  handleClick1 = () => {
-    this.setState({
-      April: calculate_rewards(customer1_april),
-      May: calculate_rewards(customer1_may),
-      June: calculate_rewards(customer1_june),
-      Total: calculate_rewards(customer1_total)
-    })
-  }
-
-  handleClick2 = () => {
-    this.setState({
-      April: calculate_rewards(customer2_april),
-      May: calculate_rewards(customer2_may),
-      June: calculate_rewards(customer2_june),
-      Total: calculate_rewards(customer2_total)
-    })
-  }
-  
-  handleClick3 = () => {
-    this.setState({
-      April: calculate_rewards(customer3_april),
-      May: calculate_rewards(customer3_may),
-      June: calculate_rewards(customer3_june),
-      Total: calculate_rewards(customer3_total)
-    })
-  }
-
-  handleClick4 = () => {
-    this.setState({
-      April: calculate_rewards(customer4_april),
-      May: calculate_rewards(customer4_may),
-      June: calculate_rewards(customer4_june),
-      Total: calculate_rewards(customer4_total)
-    })
+  handleClick = (input_cid) => {
+    if(input_cid === customers_id_list[input_cid-1]){
+      this.setState({
+        Name: uniq_cid[input_cid-1].cname,
+        April: calculate_rewards(customer_data.filter(customer => customer.cid === input_cid && customer.month === "April")),
+        May: calculate_rewards(customer_data.filter(customer => customer.cid === input_cid && customer.month === "May")),
+        June: calculate_rewards(customer_data.filter(customer => customer.cid === input_cid && customer.month === "June")),
+        Total: calculate_rewards(customer_data.filter(customer => customer.cid === input_cid))
+      })
+    }
   }
 
   render(){
   return (
     <div className="App">
-      <br/><br/>
-      <h1>Customer Reward Points</h1>        
-      <br/><br/>
-      <button onClick = {this.handleClick1}>Customer 1</button>{" "}
-      <button onClick = {this.handleClick2}>Customer 2</button>{" "}
-      <button onClick = {this.handleClick3}>Customer 3</button>{" "}
-      <button onClick = {this.handleClick4}>Customer 4</button>
-      <br/><br/>
+      <div><h1>Customer Reward Points</h1></div>
+        <div>
+          <DropdownButton id="dropdown-basic-button" title="Select Customer ID">
+            <Dropdown.Item onClick = {() => this.handleClick(customers_id_list[0])}>{customers_id_list[0]}</Dropdown.Item>
+            <Dropdown.Item onClick = {() => this.handleClick(customers_id_list[1])}>{customers_id_list[1]}</Dropdown.Item>
+            <Dropdown.Item onClick = {() => this.handleClick(customers_id_list[2])}>{customers_id_list[2]}</Dropdown.Item>
+            <Dropdown.Item onClick = {() => this.handleClick(customers_id_list[3])}>{customers_id_list[3]}</Dropdown.Item>
+          </DropdownButton>
+        </div>
+      <h2>Name: {this.state.Name}</h2>
       <h2>April: {this.state.April}</h2>
       <h2>May: {this.state.May}</h2>
       <h2>June: {this.state.June}</h2>
       <h2>Total: {this.state.Total}</h2>
-      <br/><br/>
     </div>
   );
 }
@@ -167,32 +147,14 @@ var rewards = reward(amount);
 for(let i = 0; i < customer_data.length; i++){
       customer_data[i].rewards = rewards[i];
 }
-console.log(customer_data);
-console.log(JSON.stringify(customer_data[0]));
 
-// Filtering the reward points for each month and the total
-// Customer 1
-var customer1_total = customer_data.filter(customer => customer.cid === 1);
-var customer1_april = customer_data.filter(customer => customer.cid === 1 && customer.month === "April");
-var customer1_may = customer_data.filter(customer => customer.cid === 1 && customer.month === "May");
-var customer1_june = customer_data.filter(customer => customer.cid === 1 && customer.month === "June");
+// Creating Array with unique customer id
 
-// Customer 2
-var customer2_total = customer_data.filter(customer => customer.cid === 2);
-var customer2_april = customer_data.filter(customer => customer.cid === 2 && customer.month === "April");
-var customer2_may = customer_data.filter(customer => customer.cid === 2 && customer.month === "May");
-var customer2_june = customer_data.filter(customer => customer.cid === 2 && customer.month === "June");
+var uniq_cid = customer_data.filter(function ({cid}) {
+  return !this[cid] && (this[cid] = cid);
+}, {});
 
-// Customer 3
-var customer3_total = customer_data.filter(customer => customer.cid === 3);
-var customer3_april = customer_data.filter(customer => customer.cid === 3 && customer.month === "April");
-var customer3_may = customer_data.filter(customer => customer.cid === 3 && customer.month === "May");
-var customer3_june = customer_data.filter(customer => customer.cid === 3 && customer.month === "June");
-
-// Customer 4
-var customer4_total = customer_data.filter(customer => customer.cid === 4);
-var customer4_april = customer_data.filter(customer => customer.cid === 4 && customer.month === "April");
-var customer4_may = customer_data.filter(customer => customer.cid === 4 && customer.month === "May");
-var customer4_june = customer_data.filter(customer => customer.cid === 4 && customer.month === "June");
-
-
+var customers_id_list = [];
+for( let i= 0; i < uniq_cid.length; i++){
+  customers_id_list.push(uniq_cid[i].cid);
+}
