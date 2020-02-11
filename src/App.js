3492e80bd87838e819094a5a9fad5fc16c9f2d77
Ashik Liyakathali <ashik21395@gmail.com>
2020-02-11 10:41:23 -0500

    made small changes and formatted the code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 // A retailer offers a amount program to its customers_id_list, awarding points based on each recorded purchase.
 // A customer receives 2 points for every dollar spent over $100 in each transaction,
 // plus 1 point for every dollar spent over $50 in each transaction (e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).
-// Given a record of every transaction during a three month period, 
+// Given a record of every transaction during a three month period,
 // calculate the reward points earned for each customer per month and total.
 
 // Directions:
@@ -11,10 +11,10 @@
 // · Make up a data set to best demonstrate your solution
 // · Check solution into GitHub
 
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     fields: {},
     customerID: "",
@@ -23,7 +23,7 @@ class App extends React.Component{
     May: "",
     June: "",
     Total: ""
-  }
+  };
 
   onChange = updatedValue => {
     this.setState({
@@ -33,37 +33,54 @@ class App extends React.Component{
       }
     });
   };
-  
+
   change = e => {
-    this.onChange({ [e.target.name]: e.target.value });
+    this.onChange({
+      [e.target.name]: e.target.value
+    });
     this.setState({
       [e.target.name]: e.target.value
     });
   };
 
-  handleClick = (e) => {
+  handleClick = e => {
     e.preventDefault();
-    let input_cid = this.state.customerID;
-    if(customers_id_list.indexOf(parseInt(input_cid))!== -1){
+    let input_cid = parseInt(this.state.customerID);
+    if (input_cid === customers_id_list[input_cid - 1]) {
       this.setState({
-        Name: uniq_cid[parseInt(input_cid)-1].cname,
-        April: calculate_rewards(customer_data.filter(customer => customer.cid === parseInt(input_cid) && customer.month === "April")),
-        May: calculate_rewards(customer_data.filter(customer => customer.cid === parseInt(input_cid) && customer.month === "May")),
-        June: calculate_rewards(customer_data.filter(customer => customer.cid === parseInt(input_cid) && customer.month === "June")),
-        Total: calculate_rewards(customer_data.filter(customer => customer.cid === parseInt(input_cid)))
-      }); 
-    }
-    else{
+        Name: uniq_cid[input_cid - 1].cname,
+        April: calculate_rewards(
+          customer_data.filter(
+            customer => customer.cid === input_cid && customer.month === "April"
+          )
+        ),
+        May: calculate_rewards(
+          customer_data.filter(
+            customer => customer.cid === input_cid && customer.month === "May"
+          )
+        ),
+        June: calculate_rewards(
+          customer_data.filter(
+            customer => customer.cid === input_cid && customer.month === "June"
+          )
+        ),
+        Total: calculate_rewards(
+          customer_data.filter(customer => customer.cid === input_cid)
+        )
+      });
+    } else {
       alert("Enter a valid Customer ID");
     }
   };
 
-  render(){
-   return (
-    <div className="App">
-      <div><h1>Customer Reward Points</h1></div>
+  render() {
+    return (
+      <div className="App">
         <div>
-          <form> 
+          <h1>Customer Reward Points</h1>
+        </div>
+        <div>
+          <form>
             <input
               name="customerID"
               placeholder="Enter Customer ID"
@@ -74,22 +91,32 @@ class App extends React.Component{
                 height: 30,
                 width: 200,
                 margin: 5
-                }}
-              type= "number"
+              }}
+              type="number"
             />
-           <br />
-           <button onClick={(e) => this.handleClick(e)}>Submit</button>
+            <br />
+            <button onClick={e => this.handleClick(e)}>Submit</button>
           </form>
         </div>
-     <div>
-          <h4>Name: <span>{this.state.Name}</span></h4>
-          <h4>April: <span>{this.state.April}</span></h4>
-          <h4>May: <span>{this.state.May}</span></h4>
-          <h4>June: <span>{this.state.June}</span></h4>
-          <h4>Total: <span>{this.state.Total}</span></h4>
-     </div>
-    </div>
-   );
+        <div>
+          <h4>
+            Name: <span>{this.state.Name}</span>
+          </h4>
+          <h4>
+            April: <span>{this.state.April}</span>
+          </h4>
+          <h4>
+            May: <span>{this.state.May}</span>
+          </h4>
+          <h4>
+            June: <span>{this.state.June}</span>
+          </h4>
+          <h4>
+            Total: <span>{this.state.Total}</span>
+          </h4>
+        </div>
+      </div>
+    );
   }
 }
 
@@ -97,80 +124,78 @@ export default App;
 
 // Function to calculate rewards for each transaction
 
-const reward = (amount) =>{
+const reward = amount => {
   var arr = [];
-  for(let i = 0; i < amount.length; i++){
-      if(amount[i] <= 50){
-          arr.push(0);
-      }
-      else if(amount[i] > 50 && amount[i] <= 100){
-          arr.push(amount[i] - 50);
-      }
-      else{
-          arr.push(50 + ((amount[i] - 100) * 2));
-      }
+  for (let i = 0; i < amount.length; i++) {
+    if (amount[i] <= 50) {
+      arr.push(0);
+    } else if (amount[i] > 50 && amount[i] <= 100) {
+      arr.push(amount[i] - 50);
+    } else {
+      arr.push(50 + (amount[i] - 100) * 2);
+    }
   }
   return arr;
-}
+};
 
 // Function to calculate the sum of an array
 
-const sum = (array) =>{
+const sum = array => {
   let sum = 0;
   sum = array.reduce((a, b) => a + b);
   return sum;
-}
+};
 
 // Function to calculate rewards for each filter
 
-const calculate_rewards = (array) =>{
+const calculate_rewards = array => {
   var new_array = [];
-  for( let i= 0; i < array.length; i++){
-      new_array.push(array[i].rewards);
+  for (let i = 0; i < array.length; i++) {
+    new_array.push(array[i].rewards);
   }
   var reward_points = sum(new_array);
   return reward_points;
-}
+};
 
 // Using sample transaction data set for last three months for testing the app
 
 const customer_data = [
-  {cid: 1, cname: "Ashik", month: "April", bill: 1, amount: 120},
-  {cid: 2, cname: "Dinesh", month: "April", bill: 1, amount: 150},
-  {cid: 3, cname: "Rahul", month: "April", bill: 1, amount: 180},
-  {cid: 4, cname: "Eric", month: "April", bill: 1, amount: 200},
-  {cid: 1, cname: "Ashik", month: "April", bill: 2, amount: 220},
-  {cid: 2, cname: "Dinesh", month: "April", bill: 2, amount: 250},
-  {cid: 3, cname: "Rahul", month: "April", bill: 2, amount: 280},
-  {cid: 4, cname: "Eric", month: "April", bill: 2, amount: 300},
-  {cid: 1, cname: "Ashik", month: "April", bill: 3, amount: 320},
-  {cid: 2, cname: "Dinesh", month: "April", bill: 3, amount: 350},
-  {cid: 1, cname: "Ashik", month: "May", bill: 1, amount: 120},
-  {cid: 2, cname: "Dinesh", month: "May", bill: 1, amount: 150},
-  {cid: 3, cname: "Rahul", month: "May", bill: 1, amount: 180},
-  {cid: 4, cname: "Eric", month: "May", bill: 1, amount: 200},
-  {cid: 1, cname: "Ashik", month: "May", bill: 2, amount: 220},
-  {cid: 2, cname: "Dinesh", month: "May", bill: 2, amount: 250},
-  {cid: 3, cname: "Rahul", month: "May", bill: 2, amount: 280},
-  {cid: 4, cname: "Eric", month: "May", bill: 2, amount: 300},
-  {cid: 1, cname: "Ashik", month: "May", bill: 3, amount: 320},
-  {cid: 2, cname: "Dinesh", month: "May", bill: 3, amount: 350},
-  {cid: 1, cname: "Ashik", month: "June", bill: 1, amount: 120},
-  {cid: 2, cname: "Dinesh", month: "June", bill: 1, amount: 150},
-  {cid: 3, cname: "Rahul", month: "June", bill: 1, amount: 180},
-  {cid: 4, cname: "Eric", month: "June", bill: 1, amount: 200},
-  {cid: 1, cname: "Ashik", month: "June", bill: 2, amount: 220},
-  {cid: 2, cname: "Dinesh", month: "June", bill: 2, amount: 250},
-  {cid: 3, cname: "Rahul", month: "June", bill: 2, amount: 280},
-  {cid: 4, cname: "Eric", month: "June", bill: 2, amount: 300},
-  {cid: 1, cname: "Ashik", month: "June", bill: 3, amount: 320},
-  {cid: 2, cname: "Dinesh", month: "June", bill: 3, amount: 350},
+  { cid: 1, cname: "Ashik", month: "April", bill: 1, amount: 120 },
+  { cid: 2, cname: "Dinesh", month: "April", bill: 1, amount: 150 },
+  { cid: 3, cname: "Rahul", month: "April", bill: 1, amount: 180 },
+  { cid: 4, cname: "Eric", month: "April", bill: 1, amount: 200 },
+  { cid: 1, cname: "Ashik", month: "April", bill: 2, amount: 220 },
+  { cid: 2, cname: "Dinesh", month: "April", bill: 2, amount: 250 },
+  { cid: 3, cname: "Rahul", month: "April", bill: 2, amount: 280 },
+  { cid: 4, cname: "Eric", month: "April", bill: 2, amount: 300 },
+  { cid: 1, cname: "Ashik", month: "April", bill: 3, amount: 320 },
+  { cid: 2, cname: "Dinesh", month: "April", bill: 3, amount: 350 },
+  { cid: 1, cname: "Ashik", month: "May", bill: 1, amount: 120 },
+  { cid: 2, cname: "Dinesh", month: "May", bill: 1, amount: 150 },
+  { cid: 3, cname: "Rahul", month: "May", bill: 1, amount: 180 },
+  { cid: 4, cname: "Eric", month: "May", bill: 1, amount: 200 },
+  { cid: 1, cname: "Ashik", month: "May", bill: 2, amount: 220 },
+  { cid: 2, cname: "Dinesh", month: "May", bill: 2, amount: 250 },
+  { cid: 3, cname: "Rahul", month: "May", bill: 2, amount: 280 },
+  { cid: 4, cname: "Eric", month: "May", bill: 2, amount: 300 },
+  { cid: 1, cname: "Ashik", month: "May", bill: 3, amount: 320 },
+  { cid: 2, cname: "Dinesh", month: "May", bill: 3, amount: 350 },
+  { cid: 1, cname: "Ashik", month: "June", bill: 1, amount: 120 },
+  { cid: 2, cname: "Dinesh", month: "June", bill: 1, amount: 150 },
+  { cid: 3, cname: "Rahul", month: "June", bill: 1, amount: 180 },
+  { cid: 4, cname: "Eric", month: "June", bill: 1, amount: 200 },
+  { cid: 1, cname: "Ashik", month: "June", bill: 2, amount: 220 },
+  { cid: 2, cname: "Dinesh", month: "June", bill: 2, amount: 250 },
+  { cid: 3, cname: "Rahul", month: "June", bill: 2, amount: 280 },
+  { cid: 4, cname: "Eric", month: "June", bill: 2, amount: 300 },
+  { cid: 1, cname: "Ashik", month: "June", bill: 3, amount: 320 },
+  { cid: 2, cname: "Dinesh", month: "June", bill: 3, amount: 350 }
 ];
 
 // Removing the transaction amount into a separate array to calculate the reward points
 
 var amount = [];
-for( let i= 0; i < customer_data.length; i++){
+for (let i = 0; i < customer_data.length; i++) {
   amount.push(customer_data[i].amount);
 }
 
@@ -178,17 +203,17 @@ var rewards = reward(amount);
 
 // Adding the rewards in the customer data set
 
-for(let i = 0; i < customer_data.length; i++){
-      customer_data[i].rewards = rewards[i];
+for (let i = 0; i < customer_data.length; i++) {
+  customer_data[i].rewards = rewards[i];
 }
 
 // Creating Array with unique customer id
 
-var uniq_cid = customer_data.filter(function ({cid}) {
+var uniq_cid = customer_data.filter(function({ cid }) {
   return !this[cid] && (this[cid] = cid);
 }, {});
 
 var customers_id_list = [];
-for( let i= 0; i < uniq_cid.length; i++){
+for (let i = 0; i < uniq_cid.length; i++) {
   customers_id_list.push(uniq_cid[i].cid);
 }
